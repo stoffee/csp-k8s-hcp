c81573a01d454a34cfe1c192cb35a313dd359877
stoffee <chris@roadkill.org>
2023-03-02 13:54:34 -1000

    updated versions

--- aws/vault/eks_only/main.tf
+++ aws/vault/eks_only/main.tf
@@ -1,6 +1,7 @@
 module "hcp-eks" {
   source               = "stoffee/vault-eks/hcp"
-  version              = "~> 0.0.7"
+  version              = "~> 0.0.11"
+  #source               = "../../../../terraform-hcp-vault-eks/"
   cluster_id           = var.cluster_id
   deploy_hvn           = var.deploy_hvn
   hvn_id               = var.hvn_id
@@ -10,5 +11,5 @@ module "hcp-eks" {
   deploy_eks_cluster   = var.deploy_eks_cluster
   vpc_region           = var.vpc_region
   eks_instance_types   = ["t2.small"]
-  eks_cluster_version  = "1.24"
+  eks_cluster_version  = "1.22"
 }

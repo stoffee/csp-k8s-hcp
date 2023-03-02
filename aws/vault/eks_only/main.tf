33e148a933cecc74f6d298d15012914c2359b781
stoffee <chris@roadkill.org>
2023-03-02 08:48:25 -1000

    added aws/eks-only deployment

---
+++ aws/vault/eks_only/main.tf
@@ -0,0 +1,14 @@
+module "hcp-eks" {
+  source               = "stoffee/vault-eks/hcp"
+  version              = "~> 0.0.7"
+  cluster_id           = var.cluster_id
+  deploy_hvn           = var.deploy_hvn
+  hvn_id               = var.hvn_id
+  hvn_region           = var.hvn_region
+  deploy_vault_cluster = var.deploy_vault_cluster
+  hcp_vault_cluster_id = var.hcp_vault_cluster_id
+  deploy_eks_cluster   = var.deploy_eks_cluster
+  vpc_region           = var.vpc_region
+  eks_instance_types   = ["t2.small"]
+  eks_cluster_version  = "1.24"
+}

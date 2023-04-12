eda1237ec07620920bf547433254da122adc5a48
stoffee <chris@roadkill.org>
2023-04-12 13:54:14 -0700

    added admin policy

--- aws/vault/eks_only_with_aws_secrets_engine/part2/main.tf
+++ aws/vault/eks_only_with_aws_secrets_engine/part2/main.tf
@@ -2,6 +2,71 @@ provider "vault" {
   address = var.vault_addr
   token   = var.vault_token
 }
+resource "vault_policy" "admin_policy" {
+  namespace = "admin"
+  name      = "supah-user"
+  policy    = <<EOT
+  # Manage auth methods broadly across Vault
+path "auth/*"
+{
+  capabilities = ["create", "read", "update", "delete", "list", "sudo"]
+}
+
+# Create, update, and delete auth methods
+path "sys/auth/*"
+{
+  capabilities = ["create", "update", "delete", "sudo"]
+}
+
+# List auth methods
+path "sys/auth"
+{
+  capabilities = ["read"]
+}
+
+# Create and manage ACL policies
+path "sys/policies/acl/*"
+{
+  capabilities = ["create", "read", "update", "delete", "list", "sudo"]
+}
+
+# List ACL policies
+path "sys/policies/acl"
+{
+  capabilities = ["list"]
+}
+
+# Create and manage secrets engines broadly across Vault.
+path "sys/mounts/*"
+{
+  capabilities = ["create", "read", "update", "delete", "list", "sudo"]
+}
+
+# List enabled secrets engines
+path "sys/mounts"
+{
+  capabilities = ["read", "list"]
+}
+
+# List, create, update, and delete key/value secrets at secret/
+path "secret/*"
+{
+  capabilities = ["create", "read", "update", "delete", "list", "sudo"]
+}
+
+# Manage transit secrets engine
+path "transit/*"
+{
+  capabilities = ["create", "read", "update", "delete", "list", "sudo"]
+}
+
+# Read health checks
+path "sys/health"
+{
+  capabilities = ["read", "sudo"]
+}
+EOT
+}
 
 resource "vault_token" "admin" {
   #role_name = "app"

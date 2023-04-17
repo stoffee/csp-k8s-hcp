a19491e06ec87e9dd3da13f22cdd3dadc364f9e1
stoffee <chris@roadkill.org>
2023-04-17 16:21:29 -0700

    updated consul client

--- aws/eks_vault_consul/variables.tf
+++ aws/eks_vault_consul/variables.tf
@@ -101,16 +101,6 @@ variable "namespace" {
   description = "Namespace to deploy the Consul Helm chart"
 }
 
-variable "cluster_workspace" {
-  type        = string
-  description = "Workspace that created the Kubernetes cluster"
-}
-
-variable "organization" {
-  type        = string
-  description = "Organization of workspace that created the Kubernetes cluster"
-}
-
 variable "replicas" {
   type        = number
   default     = 1

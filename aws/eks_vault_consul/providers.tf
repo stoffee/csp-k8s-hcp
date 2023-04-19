777164833d78e24478101a03505d493723560d50
stoffee <chris@roadkill.org>
2023-04-19 14:53:37 -0700

    removed root helm provider config

--- aws/eks_vault_consul/providers.tf
+++
@@ -1,7 +0,0 @@
-provider "helm" {
-  kubernetes {
-    host                   = module.hcp-eks.cluster_endpoint
-    cluster_ca_certificate = module.hcp-eks.eks_cluster_certificate_authority_data
-    token = module.hcp-eks.eks_cluster_token
-  }
-}
